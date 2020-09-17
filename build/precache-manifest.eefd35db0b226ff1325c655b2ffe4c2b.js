ead69b7b78c2af157c2bb9012a1fce0bf9d5b53a
qiang.fang <qiang.fang@huilianyi.com>
2020-09-18 00:47:28 +0800

    feat: build book

---
+++ build/precache-manifest.eefd35db0b226ff1325c655b2ffe4c2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26793fffb19c92901bfc0898f4a8167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922ad88d7e852944cf89",
+    "url": "/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "0584a109b4b85e697739",
+    "url": "/static/css/main.240c6003.chunk.css"
+  },
+  {
+    "revision": "922ad88d7e852944cf89",
+    "url": "/static/js/2.9f37b2a7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.9f37b2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0584a109b4b85e697739",
+    "url": "/static/js/main.68f09600.chunk.js"
+  },
+  {
+    "revision": "f13e8d188c9ec1570d77",
+    "url": "/static/js/runtime-main.30e7f48d.js"
+  }
+]);
